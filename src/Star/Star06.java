3237f2de5d454571bd72e070356e332093af13db
limin <liminw417@gmail.com>
2022-11-06 22:28:58 +0900

    별찍기-삼각형6

---
+++ src/Star/Star06.java
@@ -0,0 +1,19 @@
+package Star;
+
+public class Star06 {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 5; i++) {
+
+            for(int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for(int k = 8; k > i * 2 + 1; k--) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
